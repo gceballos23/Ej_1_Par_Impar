03f6e07bdacee98d83cbdeb61a34af18a4086fb9
gceballos23 <103612856+gceballos23@users.noreply.github.com>
2022-05-09 19:19:37 -0300

    Ejercicio con switch se cambio el ejercicio con una soluciòn con case

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,17 @@ let btnSend = document.getElementById("send");
 
 btnSend.addEventListener("click", () => {
   let numero: number = Number(data.value);
-  if (numero % 2 > 0) {
-    console.log("Numero Impar");
-  } else {
-    console.log("Numero Par");
-  }
+  switch (numero) { 
+    case 0:
+
+      console.log("El nro ingresado es cero");
+      break;
+    default:  
+      if (numero % 2 > 0) {
+        console.log("Numero Impar");
+      } else {
+        console.log("Numero Par");
+      
+      }
+   }    
 });
